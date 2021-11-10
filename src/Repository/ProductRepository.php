509c518cbaf5a1e0f1fc83d1eb71fa282c2c444b
alexspi <alexspi1@yandex.ru>
2021-11-10 23:35:46 +0300

    base user and product

---
+++ src/Repository/ProductRepository.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Product;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @method Product|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Product|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Product[]    findAll()
+ * @method Product[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class ProductRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Product::class);
+    }
+
+    /**
+     * @return Product[]
+     */
+    public function findAllGreaterThanPrice(int $price, bool $includeUnavailableProducts = false): array
+    {
+        // автоматически знает, что надо выбирать Продукты
+        // "p" - это псевдоним, который вы будете использовать до конца запроса
+        $qb = $this->createQueryBuilder('p')
+            ->where('p.price > :price')
+            ->setParameter('price', $price)
+            ->orderBy('p.price', 'ASC');
+
+//        if (!$includeUnavailableProducts) {
+//            $qb->andWhere('p.available = TRUE');
+//        }
+
+        $query = $qb->getQuery();
+//        var_dump($query->execute());
+//        die();
+        return $query->execute();
+
+        // чтобы получить только один результат:
+        // $product = $query->setMaxResults(1)->getOneOrNullResult();
+    }
+    // /**
+    //  * @return Product[] Returns an array of Product objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('p')
+            ->andWhere('p.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('p.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?Product
+    {
+        return $this->createQueryBuilder('p')
+            ->andWhere('p.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
